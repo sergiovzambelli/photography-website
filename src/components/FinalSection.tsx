2de7ce3a4995d520d11dba067067fa4ef3ba3082
sergiovzambelli <sergiovzambelli@gmail.com>
2025-03-04 10:21:06 +0100

    small update

--- src/components/FinalSection.tsx
+++ src/components/FinalSection.tsx
@@ -3,32 +3,24 @@ import Link from "next/link";
 const FinalSection: React.FC = () => {
   return (
     <section className="bg-off-white text-charcoal py-16 md:py-20 lg:py-40 px-4">
-
       <div className="max-w-2xl mx-auto mb-12 md:mb-18 lg:mb-24">
-        <ul className="space-y-2 flex flex-col items-center font-medium font-poppins md:text-xl lg:text-2xl"> 
-
+        <ul className="space-y-2 flex flex-col items-center font-medium font-poppins md:text-xl lg:text-2xl">
           <li>
-            <Link href="mailto:example@example.com" >
-              example@example.com
-            </Link>
+            <Link href="mailto:example@example.com">example@example.com</Link>
           </li>
 
           <li>
-            <Link href="tel:+123456789">
-              +123 456 789
-            </Link>
+            <Link href="tel:+123456789">+123 456 789</Link>
           </li>
 
           <li>
-            <Link href="https://instagram.com/yourusername">
-              @yourusername
-            </Link>
+            <Link href="https://instagram.com/yourusername">@yourusername</Link>
           </li>
         </ul>
       </div>
 
       <h2 className="text-4xl md:text-5xl lg:text-7xl font-bold text-center font-poppins">
-        reach me
+        reach out
       </h2>
     </section>
   );
